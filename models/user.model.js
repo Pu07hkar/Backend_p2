f776c4e57f1d293ff4a1be01362ea5587b326284
Pushkar <ps231040@gmail.com>
2025-01-27 21:04:25 +0530

    updated userSchema and added password hashing

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from "mongoose";
-
+import bcrypt from "bcrypt"
 const userSchema = new Schema({
     name:{
         type: String,
@@ -16,6 +16,17 @@ const userSchema = new Schema({
         required: true,
     },
 });
+
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash("password", 10);
+    next()
+    } 
+})
+
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare("password", this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 export default User;
